ba654a4495135016784e1692d7ebf94f1e577329
Sunil Chaudhary <sunil.chaudhary@amniltech.com>
2024-11-16 22:03:46 +0545

    export script

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // @ts-check
 
 const isProduction = process.env.NODE_ENV === "production";
-const outputDir = process.env.BRANCH === 'dev' ? 'dev' : '.next';
+const outputDir = process.env.BRANCH === "dev" ? "dev" : ".next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   distDir: outputDir,
   compiler: {
     reactRemoveProperties: isProduction,
